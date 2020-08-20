df75cfa79d811750197a5f9eb2b1e8b286403627
Deepak Pandey <59663280+deepakpandey01@users.noreply.github.com>
2020-08-20 18:43:42 +0530

    connected all pages with node

--- App.js
+++ App.js
@@ -16,6 +16,57 @@ app.get('/', function(req,res){
     res.render('home');
   })
 
+  app.get('/book', function(req,res){
+    // console.log(posts);
+    res.render('book');
+  })
+
+  app.get('/cancel', function(req,res){
+    // console.log(posts);
+    res.render('cancel');
+  }) 
+  
+  app.get('/modify', function(req,res){
+    // console.log(posts);
+    res.render('modify');
+  })
+  
+  
+  app.get('/view', function(req,res){
+    // console.log(posts);
+    res.render('view');
+  })
+
+  app.get('/pnr', function(req,res){
+    // console.log(posts);
+    res.render('pnr');
+  })
+
+  app.get('/confirm', function(req,res){
+    // console.log(posts);
+    res.render('confirm');
+  })
+
+  app.get('/contact', function(req,res){
+    // console.log(posts);
+    res.render('contact');
+  })
+
+  app.get('/forgetpass', function(req,res){
+    // console.log(posts);
+    res.render('forgetpass');
+  })
+
+  app.get('/login', function(req,res){
+    // console.log(posts);
+    res.render('login');
+  })
+
+  app.get('/signup', function(req,res){
+    // console.log(posts);
+    res.render('signup');
+  })
+
 
   app.listen(3000, function() {
     console.log("Server started on port 3000");
