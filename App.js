32d4877ae9bfe41f776b568f568ded4d9f8aa79b
Deepak Pandey <59663280+deepakpandey01@users.noreply.github.com>
2020-08-26 20:54:47 +0530

    login and sign up page connected to node
    verification pending

--- App.js
+++ App.js
@@ -1,7 +1,10 @@
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require('lodash');
+var mysql = require('mysql');
+var md5 = require('md5');
 
 const app = express();
 
@@ -10,60 +13,94 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "12345",
+  database : 'login',
+  multipleStatements: true
+});
 
 app.get('/', function(req,res){
-    // console.log(posts);
     res.render('home');
   })
 
   app.get('/book', function(req,res){
-    // console.log(posts);
     res.render('book');
   })
 
   app.get('/cancel', function(req,res){
-    // console.log(posts);
     res.render('cancel');
   }) 
   
   app.get('/modify', function(req,res){
-    // console.log(posts);
     res.render('modify');
   })
   
   
   app.get('/view', function(req,res){
-    // console.log(posts);
     res.render('view');
   })
 
   app.get('/pnr', function(req,res){
-    // console.log(posts);
     res.render('pnr');
   })
 
   app.get('/confirm', function(req,res){
-    // console.log(posts);
     res.render('confirm');
   })
 
   app.get('/contact', function(req,res){
-    // console.log(posts);
     res.render('contact');
   })
 
   app.get('/forgetpass', function(req,res){
-    // console.log(posts);
     res.render('forgetpass');
   })
 
+  app.get('/landing', function(req,res){
+    res.render('landing');
+  })
+
   app.get('/login', function(req,res){
-    // console.log(posts);
     res.render('login');
   })
 
+  app.post('/login', function(req,res){
+    const username=req.body.email;
+    const password=md5(req.body.pwd);
+    con.query(`select email , password from login where email='${username}' and password=aes_encrypt('${password}','${process.env.KEY}')`, (err, foundUser) => {
+      if(err) console.log("failed1");
+      else{
+          if (foundUser[0]){
+              res.render('home');
+          }
+          else{
+              res.render('login');
+          }
+      }
+   
+  });
+  })
+
   app.get('/signup', function(req,res){
-    // console.log(posts);
+    res.render('signup');
+  })
+
+  app.post('/signup', function(req,res){
+    const email1 = req.body.mail;
+    const password1 = md5(req.body.pwd1);
+    const password2 = md5(req.body.pwd2);
+    if(password1 === password2){
+      con.query(`INSERT INTO login values('${email1}',aes_encrypt('${password1}', '${process.env.KEY}'))`, (err) => {
+        if(err){
+          res.render('signup');
+        }
+        else{
+         res.render('login');
+        }
+      })
+    }
     res.render('signup');
   })
 
