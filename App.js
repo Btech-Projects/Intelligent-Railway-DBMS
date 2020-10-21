16f3c586259b63b7aec3faffd0ea37169829403b
Deepak Pandey <59663280+deepakpandey01@users.noreply.github.com>
2020-10-21 21:42:19 +0530

    swap left

--- App.js
+++ App.js
@@ -26,13 +26,19 @@ var con = mysql.createConnection({
 
 var result1 = '';
 var luser='';
+var seats=0;
+var modify='';
+var details=[];
 
 app.get('/', function(req,res){
     if(luser === ''){
       res.render('login');
     }
     else{
-      res.render('home',{ruser: luser});
+      
+        res.render('home',{ruser: luser});
+      
+      
     }
   })
 
@@ -45,7 +51,7 @@ app.get('/', function(req,res){
     var to = req.body.to;
     // con.connect(function(err) {
     //   if (err) throw err;
-      con.query(`select * from classseats where sp='${from}' and dp='${to}'`, function (err, result, fields) {
+      con.query(`select * from classseats where sp='${from}' and dp='${to}'`, function (err, result) {
         if (err) throw err;
         result1= result;
         res.render('show',{result: result});
@@ -55,7 +61,7 @@ app.get('/', function(req,res){
 
   app.post('/show', function(req,res){
     let i = req.body.list;
-    let seats=parseInt(req.body.sit[i]);
+    seats=parseInt(req.body.sit[i]);
     var stat = '';
     if(result1[i].seatsleft >= seats){
       stat = "Confirmed";
@@ -69,21 +75,121 @@ app.get('/', function(req,res){
         res.render('show',{result: result1});
       }
       else{
-       res.render('home',{ruser: luser});
+        if(stat === "WAITING"){
+          res.render('home');
+        }
+       res.render('passenger',{number: seats});
       }
     })
     alert("Booked");
-    res.render('home',{ruser: luser});
+    res.render('passenger',{number: seats});
   })
+
+  app.post('/passenger', function(req,res){
+    var det=req.body.det;
+    var query="select * from ticket where pnr=(select max(pnr) from ticket);";
+    con.query(query, function (err, result) {
+      if (err) throw err;
+      result1= result[0];
+      var query2=`INSERT INTO pnr_details(pnr,trainno, email,name,class) values(${result1.pnr},${result1.trainno},'${luser}','${det[0]}','${result1.class}');`;
+      for(var i=1;i < seats; i++){
+        query2+=`INSERT INTO pnr_details(pnr,trainno, email,name,class) values(${result1.pnr},${result1.trainno},'${luser}','${det[i]}','${result1.class}');`;
+      }
+      con.query(query2, function (err) {
+        if (err) throw err;
+        console.log(query2);
+        res.render('home',{ruser: luser});
+      });
+    });
+    res.render('home',{ruser: luser});
+  }) 
 
   app.get('/cancel', function(req,res){
     res.render('cancel');
   }) 
+
+app.post('/cancel', function(req,res){
+  var pnr=req.body.pnr;
+  let query=`select * from ticket where pnr=${pnr};`;
+  let query3=`update ticket set status='Cancelled' where pnr=${pnr};`;
+  con.query(query, (err, foundResult) => {
+    if(err) console.log(err);
+    else{
+      var new1 =foundResult[0];
+      var query2=`UPDATE classseats SET seatsleft=seatsleft+${new1.nos} where trainno=${new1.trainno} and class='${new1.class}' and sp='${new1.sp}' and dp='${new1.dp}';`;
+      // let query2=`UPDATE classseats SET seatsleft=20 where trainno=${new1.trainno} AND class='${new1.class}' AND sp='${new1.sp}' AND dp='${new1.dp}';`;
+      console.log(query2);
+      con.query(query2, (err, foundResult2) => {
+        if(err) console.log(err);
+        else{
+          con.query(query3, (err, foundResult) => {
+            if(err) console.log("failed1");
+            else{
+            res.render('home',{ruser: luser});
+          }
+        });
+      }
+      });
+    }
+ 
+});
+})
   
   app.get('/modify', function(req,res){
     res.render('modify');
   })
   
+  app.post('/modify', function(req,res){
+    var pnr=req.body.pnr;
+  let query=`select * from pnr_details where pnr=${pnr};`;
+  con.query(query, (err, foundResult) => {
+    if(err) console.log("failed1");
+    else{
+        if (foundResult){
+          // console.log(foundResult);
+          modify=foundResult[0];
+            res.render('modify2',{pnrres: foundResult});
+        }
+        else{
+            res.render('modify');
+        }
+    }
+ 
+});
+  })
+
+
+  app.post('/modify2', function(req,res){
+    var option = req.body.scripts;
+    var change= req.body.cseat;
+    let query=`select * from pnr_details where trainno=${modify.trainno} and class='${modify.class}' and seat_no=${change};`;
+    con.query(query, (err, foundResult) => {
+      if(err) console.log(err);
+      else{
+          if (foundResult[0]){
+            console.log(foundResult[0]);
+            var query5=`insert into modify_details(pnr,trainno ,email ,class ,curr_seat_no , applied_seat_no, status)
+            values (${foundResult[0].pnr}, ${foundResult[0].trainno}, '${foundResult[0].email}', '${foundResult[0].class}',${change}, ${option}, 'Pending'); `;
+            con.query(query5, (err) => {
+              if(err) console.log(err);
+              res.render('home',{ruser: luser});
+            })
+            // var query2=`update pnr_detailts a inner join pnr_details b on a.pnr <> b.pnr set a.seat_no= b.seat_no, where trainno=${modify.trainno} and name='${modify.name}' and class='${modify.class}' and seat_no=${change};`;
+            
+          }
+          else{
+            console.log('modify2 2');
+            let query2=`update pnr_details set seat_no='${change}' where trainno=${modify.trainno} and class='${modify.class}' and seat_no=${option};`;
+            con.query(query2, (err) => {
+              if(err) console.log(err);
+              res.render('home',{ruser: luser});
+            })
+          }
+      }
+   
+    });
+    // res.render('modify');
+  })
   
   app.get('/enquiry', function(req,res){
     res.render('enquiry');
